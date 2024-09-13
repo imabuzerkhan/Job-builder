eca687798fdc3c73384175c3d9bba7b627ddd2e2
imabuzerkhan <abuzerk41@gmail.com>
2024-09-13 22:13:12 +0545

    new commit

--- job builder-application/src/_components/ProtectedRoute.jsx
+++ job builder-application/src/_components/ProtectedRoute.jsx
@@ -10,6 +10,7 @@ if(isLoaded && !isSignedIn && isSignedIn !== undefined ){
   return <Navigate to='/?sign-in = true'/>
 }
 // do somecoding abuzer yaar
+// why i am doing so lazy for doing coding i dont undrsand
 
 
   return Children ;
